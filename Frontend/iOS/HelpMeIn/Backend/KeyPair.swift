8eb325191bc02c4c3a2524b02650903298463716
Lakr <git@qaq.wiki>
2026-03-26 22:40:12 +0800

    Initial Commit

---
+++ Frontend/iOS/HelpMeIn/Backend/KeyPair.swift
@@ -0,0 +1,8 @@
+//
+//  KeyPair.swift
+//  HelpMeIn
+//
+//  Created by qaq on 26/3/2026.
+//
+
+import Foundation
---
+++ Frontend/iOS/HelpMeIn/main.swift
@@ -0,0 +1,18 @@
+//
+//  HelpMeInApp.swift
+//  HelpMeIn
+//
+//  Created by qaq on 26/3/2026.
+//
+
+import SwiftUI
+
+struct HelpMeIn: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+HelpMeIn.main()

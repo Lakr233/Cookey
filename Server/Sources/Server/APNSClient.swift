7c5a044a6e6fcf89a9c6c3333c7487d1beee28d7
Lakr <git@qaq.wiki>
2026-04-01 11:56:58 +0900

    Fix Linux APNS client networking import

--- Server/Sources/Server/APNSClient.swift
+++ Server/Sources/Server/APNSClient.swift
@@ -1,5 +1,8 @@
 import Crypto
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 public actor APNSClient {
     private let configuration: APNSConfiguration
